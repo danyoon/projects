59ffaec1be1121f43acfc4567d3e6597131689c3
Daniel Yoon <zeradan@gmail.com>
2013-03-23 12:54:43 -0700

    new user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,25 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :name, :email, :password, :password_confirmation
+  has_secure_password
+
+  before_save { |user| user.email = email.downcase }
+  VALID_NAME_REGEX = /\A[\w+\-.]+\s+[\w+\-.]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
   has_many :microposts
 end
