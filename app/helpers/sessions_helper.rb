1f46868519fff0d630e9a40f86e10e4bca19ed3c
Daniel Yoon <zeradan@gmail.com>
2013-04-03 22:29:49 -0700

    feed fix

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,12 +1,4 @@
 module SessionsHelper
-  #def current_user
-    #@current_user ||= User.find_by_remember_token(cookies[:remember_token])
-  #end
-
-  def sign_out
-    current_user = nil
-    cookies.delete(:remember_token)
-  end
 
   def redirect_back_or(default)
     redirect_to(session[:return_to] || default)
