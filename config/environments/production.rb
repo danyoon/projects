4a60b5daf1cfa7c9df3e14e06a8d49082ae806ee
Daniel Yoon <zeradan@gmail.com>
2013-04-05 14:13:24 -0700

    real one

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,8 @@ FirstApp::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'thousandsoft.heroku.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
