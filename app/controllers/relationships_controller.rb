956ea0365a496493d83994f99b24ca7d3fd1bb98
Daniel Yoon <zeradan@gmail.com>
2013-04-18 12:34:01 -0700

    fixed following bug

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,17 +1,20 @@
 class RelationshipsController < ApplicationController
-  before_filter :signed_in_user
-
-  respond_to :html, :js
 
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    respond_with @user
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-    respond_with @user
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 end
