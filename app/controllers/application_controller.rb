1a09da4375966fc7577e31325079ba11ec0da7d5
Daniel Yoon <zeradan@gmail.com>
2013-05-13 01:46:46 -0700

    login mumbo jumbo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def after_sign_in_path_for(resource)
-    current_user_path
+    current_user
   end
 
   def after_sign_out_path_for(resource)
