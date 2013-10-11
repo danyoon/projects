759ad37f632bd5b1550320bebdea96903ecc4fb1
Daniel Yoon <zeradan@gmail.com>
2013-10-10 23:15:31 -0700

    heroku bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.flot
-//= require jquery.flot.pie
 //= require jvectormap
 //= require jvectormap/maps/world_merc_en
 //= require bootstrap
