d874107f2b7ee0a1dc54caa1281af616c48be0fd
Daniel Yoon <zeradan@gmail.com>
2013-10-10 22:05:42 -0700

    css

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,34 +9,31 @@ class StaticPagesController < ApplicationController
   def stanford
     @title = 'Home'
     @user = current_user
-    @hotels = Hotel.where(city:"Washington").paginate(page: params[:page])
+    @hotels = Hotel.where(city:"Palo Alto").paginate(page: params[:page])
   end
 
   def sf
     @title = 'Home'
-    @user = current_user        
+    @user = current_user
+    @hotels = Hotel.where(city:"San Francisco").paginate(page: params[:page])        
   end
 
   def lv
     @title = 'Home'
-    @user = current_user        
+    @user = current_user
+    @hotels = Hotel.where(city:"Las Vegas").paginate(page: params[:page])        
   end
 
   def nyc
     @title = 'Home'
-    @user = current_user        
+    @user = current_user
+    @hotels = Hotel.where(city:"New York").paginate(page: params[:page])        
   end
  
   def statichotels
     @user = current_user    
     @hotels = Hotel.paginate(page: params[:page])
     @title = 'Help'
-  end
-
-  def staticblurb
-    @user = current_user    
-    @users = User.paginate(page: params[:page])
-    @title = 'Mission Statement'
   end
 
   def staticcontact
