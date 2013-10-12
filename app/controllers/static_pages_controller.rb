4aa65bbe87ef0dfce53d740f58526eec06e94e92
Daniel Yoon <zeradan@gmail.com>
2013-10-11 23:37:13 -0700

    new pricing way

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -31,87 +31,106 @@ class StaticPagesController < ApplicationController
   end
 
   def hst
-    @title = 'Host Hotels'
+    @title = 'Host Hotels and Resorts'
     @user = current_user
-    @hotels = Hotel.where(city:"New York").paginate(page: params[:page])        
+    @hotels = Hotel.where(owner:"Host Hotels and Resorts (HST)").paginate(page: params[:page])        
   end
  
   def drh
     @title = 'DiamondRock Hospitality'
     @user = current_user
-    @hotels = Hotel.where(city:"New York").paginate(page: params[:page])        
+    @hotels = Hotel.where(owner:"DiamondRock Hospitality (DRH)").paginate(page: params[:page])        
   end
 
   def sho
     @title = 'Sunstone Hotel Investors'
     @user = current_user
-    @hotels = Hotel.where(city:"New York").paginate(page: params[:page])        
+    @hotels = Hotel.where(owner:"Sunstone Hotel Investors (SHO)").paginate(page: params[:page])        
   end
 
   def lho
     @title = 'LaSalle Hotel Properties'
     @user = current_user
-    @hotels = Hotel.where(city:"New York").paginate(page: params[:page])        
+    @hotels = Hotel.where(owner:"LaSalle Hotel Properties (LHO)").paginate(page: params[:page])        
   end
 
   def bee
     @title = 'Strategic Hotels and Resorts'
     @user = current_user
-    @hotels = Hotel.where(city:"New York").paginate(page: params[:page])        
+    @hotels = Hotel.where(owner:"Strategic Hotel and Resorts (BEE)").paginate(page: params[:page])        
   end
 
   def peb
     @title = 'Pebblebrook Hotel Trust'
     @user = current_user
-    @hotels = Hotel.where(city:"New York").paginate(page: params[:page])        
+    @hotels = Hotel.where(owner:"Pebblebrook Hotel Trust (PEB)").paginate(page: params[:page])        
   end
 
   def chsp
-    @title = 'Chesapeake Lodging'
+    @title = 'Chesapeake Lodging Trust'
     @user = current_user
-    @hotels = Hotel.where(city:"New York").paginate(page: params[:page])        
+    @hotels = Hotel.where(owner:"Chesapeake Lodging Trust (CHSP)").paginate(page: params[:page])        
+  end
+
+  def ahp
+    @title = 'Ashford Hospitality Prime'
+    @user = current_user
+    @hotels = Hotel.where(owner:"Ashford Hospitality Prime (AHP)").paginate(page: params[:page])        
   end
 
   def aht
-    @title = 'Ashford Hospitality'
+    @title = 'Ashford Hospitality Trust'
     @user = current_user
-    @hotels = Hotel.where(city:"New York").paginate(page: params[:page])        
+    @hotels = Hotel.where(owner:"Ashford Hospitality Trust (AHT)").paginate(page: params[:page])        
   end
 
   def rlj
-    @title = 'RLJ Lodging'
+    @title = 'RLJ Lodging Trust'
     @user = current_user
-    @hotels = Hotel.where(city:"New York").paginate(page: params[:page])        
+    @hotels = Hotel.where(owner:"RLJ Lodging Trust (RLJ)").paginate(page: params[:page])        
   end
 
-  def mgm
-    @title = 'MGM Resorts'
+  def fch
+    @title = 'Felcor Lodging Trust'
     @user = current_user
-    @hotels = Hotel.where(city:"New York").paginate(page: params[:page])        
+    @hotels = Hotel.where(owner:"Felcor Lodging Trust(FCH)").paginate(page: params[:page])        
+  end
+
+  def ht
+    @title = 'Hersha Hospitality Trust'
+    @user = current_user
+    @hotels = Hotel.where(owner:"Hersha Hospitality (HT)").paginate(page: params[:page])        
+  end
+
+  def fch
+    @title = 'Hospitality Property Trust'
+    @user = current_user
+    @hotels = Hotel.where(owner:"Hospitality Property Trust (HPT)").paginate(page: params[:page])        
+  end
+
+
+  def mgm
+    @title = 'MGM Resorts International'
+    @user = current_user
+    @hotels = Hotel.where(owner:"MGM Resorts (MGM)").paginate(page: params[:page])        
   end
 
   def lvs
-    @title = 'Las Vegas Sands'
+    @title = 'Las Vegas Sands Corp'
     @user = current_user
-    @hotels = Hotel.where(city:"New York").paginate(page: params[:page])        
+    @hotels = Hotel.where(owner:"Las Vegas Sands (LVS)").paginate(page: params[:page])        
   end
 
   def wynn
     @title = 'Wynn Resorts'
     @user = current_user
-    @hotels = Hotel.where(city:"New York").paginate(page: params[:page])        
+    @hotels = Hotel.where(owner:"Wynn Resorts (WYNN)").paginate(page: params[:page])        
   end
 
   def czr
     @title = 'Caesars Entertainment'
     @user = current_user
-    @hotels = Hotel.where(city:"New York").paginate(page: params[:page])        
-  end
-
-  def statichotels
-    @user = current_user    
-    @hotels = Hotel.paginate(page: params[:page])
-    @title = 'Help'
+    @hotels = Hotel.where(owner:"Caesars Entertainment(CZR)").paginate(page: params[:page])        
   end
 
   def staticcontact
