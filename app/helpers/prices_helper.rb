ce6b204470f40e2b042fbc4ca92cda325f442c56
paranoiase Kang <paranoiase@gmail.com>
2013-10-16 16:24:07 +0900

    output price today ~ 7 days after. apply header of hotel list table

--- app/helpers/prices_helper.rb
+++ app/helpers/prices_helper.rb
@@ -1,6 +1,10 @@
 module PricesHelper
   def average_price(hotels, params=nil)
     price = hotels.average_prices(params)
+    decorate_price(price)
+  end
+
+  def decorate_price(price)
     if price
       "$" + number_with_precision(price, precision: 2)
     else
