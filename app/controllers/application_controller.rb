ff18ca32982629e39b39a41384ad082ff37f034c
paranoiase Kang <paranoiase@gmail.com>
2013-10-16 14:47:57 +0900

    fix track/follow button bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def after_sign_in_path_for(resource)
+    case session[:redirect_type]
+    when :add_connection
+      hotel = Hotel.find(session[:redirect_value])
+      current_user.connect!(hotel)
+    end
+
+    session.delete(session[:redirect_type])
+    session.delete(session[:redirect_value])
+
     current_user
   end
 
-  def after_sign_out_path_for(resource)
-    root_url
+  def after_sign_out_path_for(resource_or_scope)
+    request.referrer
   end
 end
--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -1,8 +1,7 @@
 class ConnectionsController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :find_hotel, :authenticate_user!
 
   def create
-    @hotel = Hotel.find(params[:connection][:connected_id])
     current_user.connect!(@hotel)
     respond_to do |format|
       format.html { redirect_to :back }
@@ -11,11 +10,27 @@ class ConnectionsController < ApplicationController
   end
 
   def destroy
-    @hotel = Connection.find(params[:id]).connected
     current_user.disconnect!(@hotel)
     respond_to do |format|
       format.html { redirect_to :back }
       format.js
     end
+  end
+
+private
+  def find_hotel
+    if action_name == "create"
+      @hotel = Hotel.find(params[:connection][:connected_id])
+    else
+      @hotel = Connection.find(params[:id]).connected
+    end
+  end
+
+  def authenticate_user!
+    if action_name == "create"
+      session[:redirect_type] = :add_connection
+      session[:redirect_value] = @hotel.id
+    end
+    super
   end
 end
