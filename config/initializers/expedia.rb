4f5fe9ba911c95a616504903b2db54e28fbdda76
paranoiase Kang <paranoiase@gmail.com>
2013-10-16 16:25:03 +0900

    remove expedia key and secret

--- config/initializers/expedia.rb
+++ config/initializers/expedia.rb
@@ -1,7 +1,7 @@
 Expedia.setup do |config|
 	config.cid = 55505 # your cid once you go live.
-	config.api_key = ENV["EXPEDIA_ID"] || "f2zk2fhzen9e9pv3nkm73fc7"
-	config.shared_secret = ENV["EXPEDIA_SECRET"] || "DTAfJ7XS"
+	config.api_key = ENV["EXPEDIA_ID"]
+	config.shared_secret = ENV["EXPEDIA_SECRET"]
 	config.locale = 'en_US' # For Example 'de_DE'. Default is 'en_US'
 	config.currency_code = 'USD' # For Example 'EUR'. Default is 'USD'
 	config.minor_rev = 13 # between 4-19 as of Dec 2012. Default is 4. 19 being latest
