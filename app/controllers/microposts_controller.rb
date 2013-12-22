380dc389ccfdaf725d1bd0c70bf7bd521c33d723
Daniel Yoon <zeradan@gmail.com>
2013-12-22 14:51:44 -0800

    new files

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,4 +1,7 @@
 class MicropostsController < ApplicationController
+  def new
+    @micropost = Micropost.new
+  end
 
   def create
     @micropost = current_user.microposts.build(params[:micropost])
