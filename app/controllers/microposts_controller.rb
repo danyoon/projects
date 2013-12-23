96395799a0f7da612d5ba4d9281780081c51b7db
Daniel Yoon <zeradan@gmail.com>
2013-12-23 12:15:28 -0800

    text_field, text_area

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,23 @@
 class MicropostsController < ApplicationController
   def create
+    if params[:to].index("@") == nil
+      @user = User.find_by_name(params[:to])
+
+      if !@user
+        redirect_to :back, alert: "Can't find user named \"#{params[:to]}\""
+      else
+        redirect_to :back, notice: "Successfully sent a message to \"#{params[:to]}\""
+      end
+    else
+      arguments = {
+        from: "noreply@thousandsoft.com",
+        to: "zeradan@gmail.com",
+        subject: "From #{current_user.name}: #{params[:subject]}",
+        html: "From #{current_user.name}: #{params[:message]}"
+      }
+      Mailgun().messages.send_email(arguments)
+      redirect_to :back, notice: "Successfully send a message to \"#{params[:to]}\""
+    end
 
   end
 
@@ -10,33 +28,12 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost deleted!"
     end
     # in both cases, redirect to root_path
-    redirect_to root_url
+    redirect_to :back
   end
 
   def send_msg_form
-    @micropost = Micropost.new   
   end
 
   def send_msg
-    @micropost = Micropost.new   
-    
-    if params[:to].index("@") == nil
-      @user = User.find_by_name(params[:to])
-
-      if !@user
-        redirect_to :back, alert: "Can't find user named \"#{params[:user]}\""
-      else
-        redirect_to :back, notice: "Successfully sent a message to \"#{params[:user]}\""
-      end
-    else
-      arguments = {
-        from: "noreply@thousandsoft.com",
-        to: params[:to],
-        subject: "From #{current_user.name}: #{params[:subject]}",
-        html: "From #{current_user.name}: #{params[:message]}"
-      }
-      Mailgun().messages.send_email(arguments)
-      redirect_to :back, notice: "Successfully send a message to \"#{params[:user]}\""
-    end
   end
 end
