e78a716182132cce7f201d3aaa9187becbb5cf17
Daniel Yoon <zeradan@gmail.com>
2013-12-22 22:51:51 -0800

    text_field, text_area

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,9 +7,9 @@ class MicropostsController < ApplicationController
     @micropost = current_user.microposts.build(params[:micropost])
     arguments = {
         from: "noreply@thousandsoft.com",
-        to: "#{to}",
+        to: "#{one}",
         subject: "#{current_user.name} has sent you a message",
-        html: "#{message}"
+        html: "#{three}"
       }
     Mailgun().messages.send_email(arguments)
 
