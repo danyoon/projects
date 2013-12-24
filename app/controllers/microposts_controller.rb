e83b88b6476dc2fc3d68d7da9e79f7aaa07476dd
Daniel Yoon <zeradan@gmail.com>
2013-12-24 09:48:47 -0800

    text_field, text_area

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,22 +1,22 @@
 class MicropostsController < ApplicationController
   def create
-    if params[:to].index("@") == nil
-      @user = User.find_by_name(params[:to])
+    if params[:tofinal].index("@") == nil
+      @user = User.find_by_name(params[:tofinal])
 
       if !@user
-        redirect_to :back, alert: "Can't find user named \"#{params[:to]}\""
+        redirect_to :back, alert: "Can't find user named \"#{params[:tofinal]}\""
       else
-        redirect_to :back, notice: "Successfully sent a message to \"#{params[:to]}\""
+        redirect_to :back, notice: "Successfully sent a message to \"#{params[:tofinal]}\""
       end
     else
       arguments = {
         from: "noreply@thousandsoft.com",
         to: "zeradan@gmail.com",
-        subject: "From #{current_user.name}: #{params[:subject]}",
-        html: "From #{current_user.name}: #{params[:message]}"
+        subject: "From #{current_user.name}: #{params[:subjectfinal]}",
+        html: "From #{current_user.name}: #{params[:messagefinal]}"
       }
       Mailgun().messages.send_email(arguments)
-      redirect_to :back, notice: "Successfully send a message to \"#{params[:to]}\""
+      redirect_to :back, notice: "Successfully send a message to \"#{params[:tofinal]}\""
     end
 
   end
